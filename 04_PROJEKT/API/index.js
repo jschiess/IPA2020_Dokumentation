c9c3a191f834bf624d506d2603f77d20ce83437e
holoarcher <josiah.schiess@gmail.com>
2020-03-11 10:34:25 +0100

    fixes

--- 04_PROJEKT/API/index.js
+++ 04_PROJEKT/API/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-let app = express();
+const app = express();
 
 const PORT = 3000;
 const routes = require('./routes');
@@ -12,9 +12,10 @@ const routes = require('./routes');
 app.use(cors());
 app.use(express.json());
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use('/api', routes);
 
 
-app.listen(PORT, console.log('listening on port 2' + PORT));
+app.listen(PORT, console.log('listening on port ' + PORT));
